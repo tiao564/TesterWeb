a36d8bd0e1d35959556a395f8c9c1f9e42886d22
dtitan <tiao564@hotmail.com>
2015-07-07 16:58:58 -0600

    Adding Java script, bootstrap now done, going onto Major Tom

---
+++ public_html/js/major.js
@@ -0,0 +1,7 @@
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+var addSum = document.getElementById("add-sum");
+
+numOne.addEventListener("click", function(){
+	console.log("hi");
+});
